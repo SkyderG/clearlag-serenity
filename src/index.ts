55f5a5b54a681f928072f01ace09cb7d3262835e
SkyderG <skydergamer21@gmail.com>
2025-09-24 11:19:36 -0300

    chore: v1.0.1 (2)

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ class ClearLagPlugin extends Plugin implements PluginEvents {
   public readonly type = PluginType.Addon;
 
   public constructor() {
-    super("clearlag", "1.0.0");
+    super("clearlag", "1.0.1");
   }
 
   public onInitialize(): void {
